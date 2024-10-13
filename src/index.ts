92655637e62d2a3b8e38d08e4e1a41b98edb5d40
MarckoAj <marckoaj@gmail.com>
2024-10-13 15:00:33 -0300

    fix(TScompiler) resolvido problema de execução do TS em ambiente Node

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import dotenv from 'dotenv';
+import pool from './infrastructure/database/conection.js';
 
 dotenv.config();
+
+pool.getConnection((error, connection) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Conexão com o banco de dados bem sucedida');
+  }
+  connection.release();
+});
