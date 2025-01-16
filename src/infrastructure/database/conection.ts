2b8726e60624d0779206133c4baf6cb41a85b08c
MarckoAj <marckoaj@gmail.com>
2025-01-16 14:22:50 -0300

    feat(auth): atualizado nomeclatura para conexão com banco de dados perfillDb

--- src/infrastructure/database/conection.ts
+++ src/infrastructure/database/conection.ts
@@ -4,7 +4,7 @@ const DBHOST: string = process.env.DBHOST || 'localhost';
 const DBPORT: number = +(process.env.DBPORT || 3306);
 const DBUSER: string = process.env.DBUSER || 'root';
 const DBPASS: string = process.env.DBPASS || 'Perfill0102@';
-const DBNAME: string = process.env.DBNAME || 'auvodb';
+const DBNAME: string = process.env.DBNAME || 'perfillDb';
 
 const localConfig: PoolOptions = {
   connectionLimit: 100,
