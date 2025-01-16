61b49cf1468c6205602ef04d35a7d9ab04800341
MarckoAj <marckoaj@gmail.com>
2025-01-16 14:30:12 -0300

    feat(main) adiocionando função principal para teste de conexão e criação de tabelas do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,31 @@ import definitionDb from './infrastructure/database/definitionDb.js';
 
 dotenv.config();
 
-async function testConnection() {
+async function testConnection(): Promise<boolean> {
   try {
     const connection = await pool.getConnection();
     console.log('Conexão com o banco de dados bem sucedida');
-
     connection.release();
+    return true;
   } catch (error) {
     console.error('Erro ao conectar ao banco de dados:', error);
+    return false;
   }
 }
 
-try {
-  await definitionDb.createTables();
-} catch (error) {
-  console.log(error);
+async function main() {
+  const connectionSuccess = await testConnection();
+
+  if (connectionSuccess) {
+    try {
+      await definitionDb.init();
+      console.log('Tabelas criadas com sucesso');
+    } catch (error) {
+      console.error('Erro ao criar tabelas:', error);
+    }
+  } else {
+    console.log('Conexão ao banco de dados falhou. Criação de tabelas não executada.');
+  }
 }
 
-testConnection();
+main();
